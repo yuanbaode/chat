a702274801aeb1b1fb026a07ea31729a64d55c9b
yuanbaode <871181348@qq.com>
2019-03-30 17:16:02 +0800

    日志æinfo modeify

--- models/init.go
+++ models/init.go
@@ -5,13 +5,12 @@ import (
 	"mychatroom/log"
 	_ "github.com/go-sql-driver/mysql"
 	"os"
-	"time"
 )
 
 func InitModel() {
 	var err error
 	//sqlconn := "root:Root#123@tcp(118.25.102.107:3306)/dev_chatroom?charset=utf8"
-	sqlconn := "root:Root#123@tcp(118.25.102.107:3306)/player_api?charset=utf8"
+	sqlconn := "root:Root#123@tcp(118.25.102.107:3306)/player_api?charset=utf8&loc=Local"
 	if err = orm.RegisterDriver("mysql", orm.DRMySQL); err != nil {
 		log.Error("database register driver err:%s\n", err.Error())
 		os.Exit(1)
@@ -21,7 +20,7 @@ func InitModel() {
 		log.Error("database connect err:%s\n", err.Error())
 		os.Exit(1)
 	}
-	orm.DefaultTimeLoc = time.Local
+	//orm.DefaultTimeLoc = time.Local
 	//orm.RegisterModel(new(Room))
 	orm.RegisterModel(new(User))
 	orm.RegisterModel(new(InfoStored))
